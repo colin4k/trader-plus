67c5b9e7ca41873e2f4f8ec93aaf09a2d3be83a4
Zhu Zhonghua <colin@C20231122198403.local>
2024-10-14 20:54:12 +0800

    获取日 k 线数据

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-#然后正式开始
+#ccxt 用于访问加密货币交易所的API
 import ccxt
 import json
+#pandas 用于数据处理和分析。
+import pandas as pd
+
+#time 用于时间相关的操作。
+import time
 
 #打印ccxt里面的所有交易所
 print(ccxt.exchanges)
@@ -19,23 +24,39 @@ exchange=ccxt.binance({
 })
 
 #载入交易所的数据
-markets=exchange.load_markets()
-print(markets)
+#markets=exchange.load_markets()
+#print(markets)
 
 #获取盘口信息,盘口的信息其实是个字典，所以我们可以用使用字典的方法来调出盘口的某类信息
 symbol="BTC/USDT"
-orderbook=exchange.fetch_order_book(symbol)
-print("bids",orderbook["bids"])
-print("asks",orderbook["asks"])
+#orderbook=exchange.fetch_order_book(symbol)
+#print("bids",orderbook["bids"])
+#print("asks",orderbook["asks"])
 
 #获取price ticker数据
-if (exchange.has["fetchTicker"]):
-    print("ticker",exchange.fetch_ticker(symbol))
+#if (exchange.has["fetchTicker"]):
+#print("ticker",exchange.fetch_ticker(symbol))
 
 #获取1日k线数据
 kline=exchange.fetch_ohlcv(symbol,"1d")
-print(kline)
+#print(kline)
 
 #获取公共交易数据
-public_trade=exchange.fetch_trades(symbol)
-print(public_trade)
+#public_trade=exchange.fetch_trades(symbol)
+#print(public_trade)
+
+# 先将获取的k线数据转成一个二维数组，它们的列名称是'time', 'open', 'high', 'low', 'close', 'vol'
+kline_df = pd.DataFrame(kline, columns=['time', 'open', 'high', 'low', 'close', 'vol'])
+
+#将 'time' 列从毫秒级时间戳转换为Pandas的datetime对象，并设置为kline_df的索引。
+kline_df.index = pd.to_datetime(kline_df['time'], unit='ms')
+
+# 使用 time.localtime() 将毫秒级时间戳转换为本地时间，
+# 并将这些本地时间作为新列 'localminute' 添加到kline_df中。
+kline_df.loc[:, "localminute"] = kline_df['time'].apply(lambda x: time.localtime(int(x/1000)))
+
+# 将本地时间分钟转换为可读的字符串格式时间戳
+kline_df.loc[:, 'timestamp'] = kline_df['localminute'].apply(lambda x: time.strftime("%Y-%m-%d %H:%M:%S", x))
+
+# 显示结果
+print(kline_df)
